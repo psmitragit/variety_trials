36997938f69a96dd14e099f21605f53597ce83b2
webgrity148 <webgrity148@gmail.com>
2024-09-11 15:10:11 +0530

    added service worker

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -14,4 +14,14 @@ $(function () {
         }
         Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+          }, function(err) {
+            console.log('Service Worker registration failed:', err);
+          });
+        });
+      }
+      
 })
